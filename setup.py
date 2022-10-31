6eed1476612853fa6471fef64d659b8a0098dcbb
AntonHome <antonrudenko2015@yandex.ru>
2022-10-31 12:45:05 +0300

    init

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     #    # "": ""
     # },
     include_package_data=True,
-    data_files = [('', ['*.dll'])],
+    data_files = [('', ['FirebirdLiteQuery.dll', 'FirebirdSql.Data.FirebirdClient.dll', 'Newtonsoft.Json.dll'])],
     license='MIT',
     packages=['firebird_lite_query'],
     install_requires=['requests'],
