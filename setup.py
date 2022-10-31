5213dedda1e1812d2e33b1ed364e5f4cf738690f
AntonHome <antonrudenko2015@yandex.ru>
2022-10-31 12:32:13 +0300

    init

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setuptools.setup(
     #    # "": ""
     # },
     license='MIT',
-    packages=['PyFirebirdLiteQuery', 'pythonnet'],
+    packages=['firebird_lite_query'],
     install_requires=['requests'],
 )
