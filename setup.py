f3a431d2de6001fd0681ffd7b987fb4756fdc0c6
AntonHome <antonrudenko2015@yandex.ru>
2022-10-31 12:27:19 +0300

    init

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ setuptools.setup(
     long_description="The library is a wrapper for api access to the database on the Firebird DBMS platform (Interbase), which returns the query result in JSON format. You just need to specify the path to the database, user access data (optional) and form an SQL query.",
     long_description_content_type="text/markdown",
     url='https://github.com/Anton198632/PyFirebirdLiteQuery',
-    project_urls = {
-       # "": ""
-    },
+    # project_urls = {
+    #    # "": ""
+    # },
     license='MIT',
     packages=['PyFirebirdLiteQuery', 'pythonnet'],
     install_requires=['requests'],
