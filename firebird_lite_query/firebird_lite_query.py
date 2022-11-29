16ce78a4ab085f900e1099dcd97d479ef2482ace
AntonHome <antonrudenko2015@yandex.ru>
2022-11-29 12:34:12 +0300

    added execution blocking for queuing parallel transactions

--- firebird_lite_query/firebird_lite_query.py
+++ firebird_lite_query/firebird_lite_query.py
@@ -1,10 +1,12 @@
 import os
+import threading
 
 import clr
 current_path = os.path.dirname(os.path.abspath(__file__))
 clr.AddReference(f'{current_path}//FirebirdLiteQuery.dll')
 import FirebirdLiteQuery
 
+lock = threading.Lock()
 
 def connect(server, db_path, username = None, pasword = None):
     FirebirdLiteQuery.Connector.Connect(server, db_path, username, pasword)
@@ -23,4 +25,17 @@ def execute_non_query(sql: str, parameters: dict = None) -> str:
         for key in parameters:
             parameters_dict.Add(key, parameters.get(key))
         return FirebirdLiteQuery.Executor.ExecuteNonQuery(sql, parameters_dict)
+
+
+def lock_execute(function, sql, parameters: dict = None):
+    m_type = function.__name__
+    result = None
+    with lock:
+        result = []
+        if m_type == 'execute_reader':
+            for r in function(sql):
+                result.append(r)
+        elif m_type == 'execute_non_query':
+            result = function(sql, parameters)
+    return result
 
